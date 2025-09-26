32ffcbe324e0b87ce033a7e477888aa2d7a31b74
Christopher-Emunah-Kim <hashin.chris@gmail.com>
2025-09-26 14:29:13 +0900

    <feat> add Dungeon content classess

---
+++ Main/Content/TheDungeon/DungeonContent.cpp
@@ -0,0 +1,29 @@
+﻿#include "DungeonContent.h"
+
+void DungeonContent::BindInput()
+{
+}
+
+void DungeonContent::ResetGame()
+{
+}
+
+void DungeonContent::ReqeustExitToTitle()
+{
+}
+
+void DungeonContent::OnInit()
+{
+}
+
+void DungeonContent::OnRelease()
+{
+}
+
+void DungeonContent::OnUpdate()
+{
+}
+
+void DungeonContent::OnRender()
+{
+}
---
+++ Main/Content/TheDungeon/DungeonContent.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+#include "../../IContent.h"
+#include "DungeonGameState.h"
+#include "DungeonRenderer.h"
+
+class DungeonContent final : public IContent
+{
+public:
+	DungeonContent() = default;
+	virtual ~DungeonContent() noexcept = default;
+
+private:
+	void BindInput();
+	void ResetGame();
+	void ReqeustExitToTitle();
+
+public:
+	virtual void OnInit() override;
+	virtual void OnRelease() override;
+	virtual void OnUpdate() override;
+	virtual void OnRender() override;
+
+private:
+	DungeonGameState m_gameState;
+	DungeonRenderer m_renderer;
+	bool m_bRequestExitToTitle = false;
+};
+
